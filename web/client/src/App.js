f5ce19870d8833b4b8c47083dbc6306989985123
Madeleine Penner <61814446+madpenner@users.noreply.github.com>
2023-11-24 14:26:12 -0800

    added some text and stuff to front end

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -2,12 +2,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import * as PropTypes from "prop-types";
 
 //port that our backend uses
 const PORT = process.env.REACT_APP_API_PORT;
 const URL = "http://localhost:" + PORT + "/api"; //URL of our backend
 
 console.log("API URL is: " + URL);
+/*
+
+function Button(props) {
+  return null;
+}
+*/
+
+Button.propTypes = {
+  onPress: PropTypes.any,
+  color: PropTypes.string,
+  title: PropTypes.string,
+  accessibilityLabel: PropTypes.string
+};
+
 function App() {
   const [data, setData] = React.useState(null);
   React.useEffect(() => {
@@ -19,10 +34,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <h1> Community Garden Login Page </h1>
+          <Toolbar
+              onLogin={() => alert('Loging in!')}
+          />
         <img src={logo} className="App-logo" alt="logo" />
         <p>{data ? (data.connected ? 'Connected' : 'Not Connected') : 'Loading...'}</p>
       </header>
     </div>
   );
 }
+
+function Toolbar({ onLogin}) {
+  return (
+      <div>
+          <h3> Enter email </h3>
+          <input name="myInput" />
+        <Button onClick={onLogin}>
+          LOGIN!
+        </Button>
+      </div>
+  );
+}
+
+function Button({ onClick, children }) {
+  return (
+      <button onClick={onClick}>
+        {children}
+      </button>
+  );
+}
+
+
 export default App;
